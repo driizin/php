716e65b8f9b55d0110212c4afe88d506600f599d
Adriel <130498577+driizin@users.noreply.github.com>
2023-10-17 16:22:13 -0300

    Add files via upload

---
+++ aula01/ex01.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $txt= "Hello World";
+    $x= 5;
+    $y= 10.5;
+    $soma= $x + $y;
+    echo "$txt, a minha variavéis x = $x e y = $y são essas, somando elas é igual $soma<br> <br>";
+    
+    ?>
+
+    <?php
+    $curso = "WEB Médio - Aluno 10!";
+    $disciplina = "PSW1";
+    $turma = 2022;
+    echo "[CURSO]: $curso<br>";
+    echo "[DISCIPLINA]: $disciplina<br>";
+    echo "[TURMA]: $turma";
+    ?>
+</body>
+</html>
